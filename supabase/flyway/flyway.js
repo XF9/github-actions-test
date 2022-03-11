e45fa14afb527ee857c8a6c317d18a0a73e87a03
Michael Möhler <m.moehler@xf9.de>
2022-03-11 22:11:33 +0100

    Use env for migrations

--- supabase/flyway/flyway.js
+++ supabase/flyway/flyway.js
@@ -1,11 +1,23 @@
-const config = require('./config.js');
+require('dotenv').config();
+
+if (!process.env.MIGRATIONS_POSTGRES_URL) {
+    throw new Error("Please define the MIGRATIONS_POSTGRES_URL environment variable inside .env");
+}
+
+if (!process.env.MIGRATIONS_POSTGRES_USER) {
+    throw new Error("Please define the MIGRATIONS_POSTGRES_USER environment variable inside .env");
+}
+
+if (!process.env.MIGRATIONS_POSTGRES_PASSWORD) {
+    throw new Error("Please define the MIGRATIONS_POSTGRES_PASSWORD environment variable inside .env");
+}
 
 module.exports = function() {
     return {
         flywayArgs: {
-            url: `jdbc:postgresql://${config.db_host}/`,
-            user: config.db_user,
-            password: config.db_password,
+            url: `jdbc:postgresql://${process.env.MIGRATIONS_POSTGRES_URL}/`,
+            user: process.env.MIGRATIONS_POSTGRES_USER,
+            password: process.env.MIGRATIONS_POSTGRES_PASSWORD,
 
             locations: 'filesystem:supabase/migrations/',
             sqlMigrationSuffixes: '.sql',
